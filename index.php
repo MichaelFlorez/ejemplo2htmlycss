5fb6f7e248814946fcc44e7486f39157c8aadc6a
Michael Florez Rico <michael.florez.r@gmail.com>
2020-10-23 14:11:32 -0500

    se agrega tipografia de terminator para el titulo

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+            Ejemplo 2 html y css
+        </title>
+        <link rel="stylesheet" type="text/css" href="css/estilos.css">
+    </head>
+    <body>
+        <!-- Esto es un título-->
+        <h1>Biografía de álguien</h1>
+
+        <img id="imagen1" src="images/homero.png"  width="250" height="350">
+
+    </body>
+</html>
